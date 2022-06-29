44ce5f8a6f791b20e281bd1a4e038bd7cc41e02d
董立洋 <541717868@qq.com>
2022-06-29 10:05:57 +0800

    Create strop.hpp

---
+++ strop.hpp
@@ -0,0 +1,26 @@
+/**
+* @ breaf 分割字符串
+* @param s 字符串
+* @param delim 分割字符
+* @node
+*/
+std::vector<std::string> vStringSplit(const std::string& s, const std::string& delim)
+{
+    std::vector<std::string> elems;
+    size_t pos = 0;
+    size_t len = s.length();
+    size_t delim_len = delim.length();
+    if (delim_len == 0) return elems;
+    while (pos < len)
+    {
+        int find_pos = s.find(delim, pos);
+        if (find_pos < 0)
+        {
+            elems.push_back(s.substr(pos, len - pos));
+            break;
+        }
+        elems.push_back(s.substr(pos, find_pos - pos));
+        pos = find_pos + delim_len;
+    }
+    return elems;
+}
